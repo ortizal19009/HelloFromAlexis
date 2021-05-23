cd9f065d3b7bf60740ec5b6616191b6ef88d5746
Mari Lopes <marieiterer@gmail.com>
2021-05-22 22:26:51 -0300

    Line added

--- src/Ortiz/Main.java
+++ src/Ortiz/Main.java
@@ -5,7 +5,10 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         String name = "Alexis";
+        String mariName= "Mariana";
         String message = "Hello, my name is %s";
         System.out.println(message.format(message, name));
+        System.out.println("Hello " + name + ", my name is " + mariName);
+
     }
 }
